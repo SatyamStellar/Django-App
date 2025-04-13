fbb9a99db346c3ecd692f7654feb19601d9af621
Satyam Stellar <satyam321tomar@gmail.com>
2025-04-13 23:16:08 +0530

    backend url

--- frontend/src/components/LoadingIndicator.jsx
+++ frontend/src/components/LoadingIndicator.jsx
@@ -1,9 +1,9 @@
-import "../styles/LoadingIndicator.css"
-
 const LoadingIndicator = () => {
-    return <div className="loading-container">
-        <div className="loader"></div>
+  return (
+    <div className="flex justify-center items-center py-4">
+      <div className="w-8 h-8 border-4 border-t-indigo-500 border-gray-600 rounded-full animate-spin"></div>
     </div>
-}
+  );
+};
 
-export default LoadingIndicator
+export default LoadingIndicator;
